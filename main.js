d2704db2cb3f38acf2f7cb3af54d6e93946455df
Moh20All <lotfiallali222@gmail.com>
2023-02-08 23:42:02 +0100

    Video section

--- main.js
+++ main.js
@@ -2,18 +2,33 @@ mypic1 = document.getElementById("img1");
 mypic2 = document.getElementById("img2");
 mypic3 = document.getElementById("img3");
 
-console.log(mypic1)
-console.log(mypic2)
-console.log(mypic3)
+
 
 mypic1.onmouseover = () => {
 
     setTimeout(function () {
         mypic1.src = "walter1.png"
-    }, 1000);
+    }, 500);
+    setTimeout(function () {
+        mypic1.src = "walter1.png"
+    }, 500);
     setTimeout(function () {
         mypic1.src = "walter2.png"
-    }, 1000);
+    }, 500);
 
 
+}
+
+function openWin(gg) {
+    myWindow = window.open(gg, "_blank");   // Opens a new window
+}
+
+document.querySelectorAll(".BB-sc .vid-container .vid video").forEach(vid => {
+    vid.onclick = () => {
+        document.querySelector(".popup").style.display = "block";
+        document.querySelector(".popup video").src = vid.getAttribute("src");
+    }
+});
+document.querySelector(".popup span").onclick = () => {
+    document.querySelector(".popup").style.display = "none";
 }
